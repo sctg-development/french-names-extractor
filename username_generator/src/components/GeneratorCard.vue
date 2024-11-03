7a03ab346633b3e0dc0bcdaf55fae9c0480033c6
æLtorio <aeltorio@universidadatlantico.org>
2024-11-03 18:53:13 +0100

    performance improvement

--- username_generator/src/components/GeneratorCard.vue
+++ username_generator/src/components/GeneratorCard.vue
@@ -9,9 +9,9 @@
       </div>
       <div class="px-6 pt-4 pb-2">
         <button class="m-1 text-sm bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          @click="firstName = normalize(randomFirstnamePonderated(firstNames))">Change first name</button>
+          @click="firstName = normalize(randomFirstnamePonderated(firstNames,preComputedTotalFirstnames))">Change first name</button>
         <button class="m-1 text-sm bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          @click="lastName = normalize(randomLastnamePonderated(lastNames))">Change last name</button>
+          @click="lastName = normalize(randomLastnamePonderated(lastNames,preComputedTotalLastnames))">Change last name</button>
         <button class="m-1 text-sm bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           @click="generateUsername">Generate username</button>
       </div>
@@ -20,14 +20,20 @@
   
   <script setup lang="ts">
   import { onMounted, ref } from 'vue';
-  import { firstNames, lastNames, normalize, randomFirstnamePonderated, randomLastnamePonderated } from '../generator';
+  import { firstNames, lastNames, normalize, precomputeCumulativeSums, randomFirstnamePonderated, randomLastnamePonderated } from '../generator';
   
   const firstName = ref('');
   const lastName = ref('');
+  const preComputedTotalFirstnames = ref(0);
+  const preComputedTotalLastnames = ref(0);
   
   function generateUsername() {
-    firstName.value = normalize(randomFirstnamePonderated(firstNames));
-    lastName.value = normalize(randomLastnamePonderated(lastNames));
+    preComputedTotalFirstnames.value = precomputeCumulativeSums(firstNames.firstnames);
+    preComputedTotalLastnames.value = precomputeCumulativeSums(lastNames.lastnames);
+    console.log(`preComputedTotalFirstnames: ${preComputedTotalFirstnames.value}`);
+    console.log(`preComputedTotalLastnames: ${preComputedTotalLastnames.value}`);
+    firstName.value = normalize(randomFirstnamePonderated(firstNames,preComputedTotalFirstnames.value));
+    lastName.value = normalize(randomLastnamePonderated(lastNames,preComputedTotalLastnames.value));
   }
   onMounted(() => {
     generateUsername();
